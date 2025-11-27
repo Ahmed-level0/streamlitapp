488d6e340b6ce23a3c504b086b86ed8c844953cd
Ahmed Hambuta <144140009+Ahmed-level0@users.noreply.github.com>
2025-11-27 02:59:34 +0200

    Eliminate temporary file creation for images
    Removed temporary file saving for uploaded images.

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from gradio_client import Client
 from PIL import Image
-import tempfile
 
 st.title("Gradio API Image Classifier")
 
@@ -12,11 +11,6 @@ uploaded = st.file_uploader("Upload Image", type=["png", "jpg", "jpeg"])
 if uploaded is not None:
     img = Image.open(uploaded)
     st.image(img, caption="Uploaded Image", use_column_width=True)
-
-    # Save to a temporary file
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmp:
-        img.save(tmp.name)
-        temp_path = tmp.name
 
     # Call Gradio API
     result = client.predict(
